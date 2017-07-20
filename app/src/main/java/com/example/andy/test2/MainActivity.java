f356877e839239d2c2b99610a079677fe5ec30d2
Andy <wuandy73@gmail.com>
2017-07-20 23:37:09 +0800

    123xxx

--- app/src/main/java/com/example/andy/test2/MainActivity.java
+++ app/src/main/java/com/example/andy/test2/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = new Button(this);
+        Button btn = new Button(this);// xxx
 
         TextView tv = new TextView(this);
     }
