cce309d4630acd2d01ae82af57e64b9d3f545e7b
Andy <wuandy73@gmail.com>
2017-07-23 20:37:41 +0800

    Util

--- app/src/main/java/com/example/andy/test2/MainActivity.java
+++ app/src/main/java/com/example/andy/test2/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.andy.test2;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
@@ -59,5 +62,22 @@ public class MainActivity extends AppCompatActivity {
             //debuggable variable will remain false
         }
         return debuggable;
+    }
+
+    /**
+     * Show hont message
+     */
+    public static void showHintDialog(Activity activity, String title, String message) {
+
+        new AlertDialog.Builder(activity)
+                .setTitle(title)
+                .setCancelable(false)
+                .setMessage(message)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
     }
 }
